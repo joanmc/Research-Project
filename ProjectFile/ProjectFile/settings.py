faa48590f31cb92fe46495e8cf6b125a2380ae3c
EllenRushe <ellen.rushe@ucdconnect.ie>
2016-06-28 17:19:47 +0100

    added root directory for static files.

--- ProjectFile/ProjectFile/settings.py
+++ ProjectFile/ProjectFile/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# Reference: tutorial.djangogirls.org/en/django_start_project/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
