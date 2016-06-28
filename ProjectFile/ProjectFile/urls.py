c288134a346f8ca33917be899b4c331620a98774
EllenRushe <ellen.rushe@ucdconnect.ie>
2016-06-28 20:08:08 +0100

    Setting up app for occupancy and test index page

--- ProjectFile/ProjectFile/urls.py
+++ ProjectFile/ProjectFile/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+# Django Tutorial for Beginners - 5 - Views, thenewboston, YouTube
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^occupancy/', include('occupancy.urls')),
 ]
