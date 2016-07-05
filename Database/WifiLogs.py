6e5b7bf5f6f7c03b4b06e06fa36df4a580124646
Joan McCarthy <joan.mc-carthy@ucdconnect.ie>
2016-07-05 13:34:29 +0100

    wifi log files - updated

--- Database/WifiLogs.py
+++ Database/WifiLogs.py
@@ -3,18 +3,26 @@ import MySQLdb as db
 from datetime import datetime
 import os
 
+# DATETIME format: YYYY-MM-DD HH:MI:SS.
+
 # table class 
 class data():
     def __init__(self, name):
+        # Connect to Database
         self.con = db.connect(host="localhost", user="root", passwd='', db=name)
         self.cursor = self.con.cursor()
         
     def createTable(self, tname):
-        sql = "CREATE TABLE IF NOT EXISTS " + tname + " (Campus VARCHAR(30), Building VARCHAR(40), Room VARCHAR(20), DateTime datetime, Associated int, Authenticated int)"
+        # Create a table
+        sql = "CREATE TABLE " + tname + " (Campus VARCHAR(30), Building VARCHAR(40), Room VARCHAR(20), DateTime datetime, Associated int, Authenticated int)"
         self.cursor.execute(sql)
         
     def openFile(self, tname, fname):
+        # Find keyworrd 'key' in csv file and read into data frame from there
         with open(fname) as f:
+            # f is the file name, starting on row 1
+            # Accounts for csvs where 'key' is in a different row
+            # num = counter
             for num, line in enumerate(f,1):
                 if line.startswith('Key'):
                     break
@@ -23,19 +31,24 @@ class data():
     
     def fixFile(self, df):
         for i in range(0, len(df)):
+            # put time into sql format
             df['Event Time'][i] = df['Event Time'][i].replace('GMT+00:00','')
             df['Event Time'][i] = datetime.strptime(df['Event Time'][i], '%a %b %d %X %Y')
+            # Split column Key (contains campus, building and room) into separate parts so they can be added to separate columns of database table
             df['Key'][i] = df['Key'][i].split(' > ')
         return df
         
-    def addFile(self, tname, df):     
+    def addFile(self, tname, df):
+        # put data from dataframe into table in database
         for i in range(0, len(df)):
-            self.cursor.execute("INSERT INTO " + tname + "(Campus, Building, Room, DateTime, Associated, Authenticated) VALUES('%s', '%s', '%s', '%s', %d, %d)" % (df['Key'][i][0], df['Key'][i][1], df['Key'][i][2], df['Event Time'][i], df['Associated Client Count'][i], df['Authenticated Client Count'][i]))
+            self.cursor.execute("INSERT INTO " + tname + "(Campus, Building, Room, DateTime, Associated, Authenticated) VALUES('%s', '%s', '%s', '%s', %d, %d)" 
+                                % (df['Key'][i][0], df['Key'][i][1], df['Key'][i][2], df['Event Time'][i], df['Associated Client Count'][i], 
+                                 df['Authenticated Client Count'][i]))
             self.con.commit()           
         return
 
 
-name = 'WiFiData'
+name = 'Data_Tuesday'
 myData = data(name)
 tname = 'WiFiLogData'
 myData.createTable(tname)
@@ -49,5 +62,3 @@ for fn in os.listdir(path):
         df = myData.openFile(tname, fname)
         df = myData.fixFile(df)
         myData.addFile(tname, df)
-
-# DATETIME - format: YYYY-MM-DD HH:MI:SS.
