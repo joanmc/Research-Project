c6066d7ffb0ae85e645c9fe72294df6055065c5b
barrykidney <barry.kidney@ucdconnect.ie>
2016-07-13 00:49:09 +0100

    template and queries changed

--- ProjectFile/occupants/views.py
+++ ProjectFile/occupants/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Rooms
 from .models import Timemodule
+from django.db.models import Q
+import datetime
 
 def login(request):
 	return render(request, 'occupants/login.html', {})
 
 def homepage(request):
-	timeMod1 = Timemodule.objects.filter(room = "B-003")
+	start_time = datetime.date(2015, 11, 1)
+	end_time = datetime.date(2015, 11, 7)
+
+	timeMod1 = Timemodule.objects.filter(room='B-004', datetime__range=(start_time, end_time))
+	
+	var_time = 9	
+
 	return render(request, 'occupants/homepage.html', {'timeMod1': timeMod1})
