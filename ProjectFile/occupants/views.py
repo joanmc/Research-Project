134dd08c4fdfd7ef1a0d2557e9f6e16389fe811c
barrykidney <barry.kidney@ucdconnect.ie>
2016-07-13 01:07:26 +0100

    views updated

---
+++ ProjectFile/occupants/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Rooms
+from .models import Timemodule
+from django.db.models import Q
+import datetime
+
+def login(request):
+	return render(request, 'occupants/login.html', {})
+
+def homepage(request):
+	start_time = datetime.date(2015, 11, 1)
+	end_time = datetime.date(2015, 11, 7)
+
+	timeMod1 = Timemodule.objects.filter(room='B-004', datetime__range=(start_time, end_time))
+	
+	var_time = 9	
+
+	return render(request, 'occupants/homepage.html', {'timeMod1': timeMod1})
