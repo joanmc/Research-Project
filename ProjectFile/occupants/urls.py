0b31d67360dd4bca358debca28c1f2c71b8be6c6
barrykidney <barry.kidney@ucdconnect.ie>
2016-07-18 16:37:46 +0100

    multiple return json fixed

--- ProjectFile/occupants/urls.py
+++ ProjectFile/occupants/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     url(r'^$',views.login, name='login'),
     url(r'^homepage/$', views.homepage, name='homepage'),
+    url(r'^graphGen$', views.graphGen, name='graphGen'),
     url(r'^test$', views.test, name='test'),
 ]
