676b28722315a31cf601a27322f40d256aec452d
barrykidney <barry.kidney@ucdconnect.ie>
2016-08-02 15:31:31 +0100

    reupload files

---
+++ ProjectFile/occupants/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+
+    url(r'^$',views.login, name='login'),
+    url(r'^homepage/$', views.homepage, name='homepage'),
+    url(r'^calendarGen$', views.calendarGen, name='calendarGen'),
+    url(r'^GenGraph$', views.GenGraph, name='GenGraph'),
+    url(r'^WiFiData$', views.WiFiData, name='WiFiData'),
+    url(r'^RoomDayGraph$', views.RoomDayGraph, name='RoomDayGraph'),
+]
